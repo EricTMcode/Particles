95103c8da1cdc06b1065a0626b630f08bd1b11a5
Eric <ebagnaro@gmail.com>
2024-01-12 15:04:09 +0100

    Initial Commit

---
+++ Particles/ParticlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParticlesApp.swift
+//  Particles
+//
+//  Created by Eric on 12/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ParticlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
